d1c58286e96669bafb40164ffa606694649782c7
p.kurdikov <p.kurdikov@ts-soft.ru>
2014-01-20 09:59:13 +0400

    TableAttribute - add constructor with name

--- TsSoft.Dapper.QueryBuilder/Metadata/TableAttribute.cs
+++ TsSoft.Dapper.QueryBuilder/Metadata/TableAttribute.cs
@@ -4,6 +4,16 @@ namespace TsSoft.Dapper.QueryBuilder.Metadata
 {
     public class TableAttribute : Attribute
     {
+        public TableAttribute()
+        {
+            
+        }
+
+        public TableAttribute(string name)
+        {
+            Name = name;
+        }
+
         public string Name { get; set; }
     }
 }
